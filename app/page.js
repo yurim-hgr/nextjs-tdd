cb36855bb027ba3e1ba16649027532f3cad0c08d
yurim-hgr <dbfla4618@naver.com>
2024-02-22 10:33:12 +0900

    feat: 폴더 구조 수정

--- app/page.js
+++ app/page.js
@@ -1,21 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import NewMessageForm from "./modules/NewMessageForm";
-import MessageList from "./modules/MessageList";
-import { useState } from "react";
-
 export default function Home() {
-  const [messages, setMessages] = useState([]);
-  function handleSend(newMessage) {
-    setMessages([newMessage, ...messages]);
-  }
-
-  return (
-    <>
-      <NewMessageForm onSend={handleSend} />
-      <MessageList data={messages} />
-    </>
-  );
+  return <div>home</div>;
 }
