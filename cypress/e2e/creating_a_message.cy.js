56347c7cfbc3432ccf9caf79f7d4b816d2b7d8e3
yurim-hgr <dbfla4618@naver.com>
2024-02-23 17:16:05 +0900

    fix: cypress config파일 base url 수정, jest act 추가

--- cypress/e2e/creating_a_message.cy.js
+++ cypress/e2e/creating_a_message.cy.js
@@ -1,6 +1,6 @@
 describe("Creating a message", () => {
   it("Displays the message in the list", () => {
-    cy.visit("/");
+    cy.visit("/message");
 
     cy.get('[data-testid="messageText"]').type("New message");
 
