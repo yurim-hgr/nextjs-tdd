b9fbaf0779b9532500233476d2cef9c1e81b222c
yurim-hgr <dbfla4618@naver.com>
2024-02-26 12:58:36 +0900

    ci: bebel 제거

--- app/message/modules/NewMessageForm.spec.js
+++ app/message/modules/NewMessageForm.spec.js
@@ -2,7 +2,7 @@
 
 import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import NewMessageForm from "./NewMessageForm";
+import NewMessageForm from "@/app/message/modules/NewMessageForm";
 
 describe("<NewMessageForm />", () => {
   describe("clicking the send button", () => {
